718cccf171218b56901d0e95e01a00b8f0bffa80
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-11-13 01:39:35 +0800

    preprocess: handle Married column and Dependents column
    Performed label encoding (binary) to Married column where 1 is Yes and 0 is No
    
    Replaced 3+ to 3 for consistency
    
    Converted the dtype from object to float64 temporarily to safely handle missing values and non-numeric values
    
    Applied mapping to validate inputs for Dependents column
    
    Filled the missing values with mode
    
    Converted the column to int

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,15 +2,33 @@ import pandas as pd
 
 # Load data
 df = pd.read_csv("data/loan_data.csv")
-print(df.head())
-
+# print(df.head())
 # Check missing values
 # print(df.isnull().sum())
-
 # print(df['Gender'].value_counts())
 
-# Handle Gender column
+# Gender column
 df['Gender'] = df['Gender'].map({'Male': 1, 'Female': 0})
 df['Gender'] = df['Gender'].fillna(df['Gender'].mode()[0]).astype('int')
 
-print(df.info())
+# Married column
+df['Married'] = df['Married'].map({'Yes': 1, 'No': 0}).astype('int')
+
+# Dependents column
+def validate_dependents(x):
+    if pd.isnull(x):
+        return None  # leave NaN for now
+    if x > 3:
+        return 3
+    elif x < 0:
+        return 0
+    else:
+        return x  # IMPORTANT: return x for normal values
+
+df['Dependents'] = df['Dependents'].replace('3+', 3)
+df['Dependents'] = df['Dependents'].astype('float64')
+df['Dependents'] = df['Dependents'].map(validate_dependents)
+df['Dependents'] = df['Dependents'].fillna(df['Dependents'].mode()[0])
+df['Dependents'] = df['Dependents'].astype('int64')
+
+# print(df.info())
