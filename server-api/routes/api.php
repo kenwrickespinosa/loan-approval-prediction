4eca59e1a5bee87b42e403dd7b1e2a6f400eaae1
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-11-18 06:25:11 +0800

    feat(route): add route
    Implemented route for login and logout

--- server-api/routes/api.php
+++ server-api/routes/api.php
@@ -5,13 +5,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+// });
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
