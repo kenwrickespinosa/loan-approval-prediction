95304f9f3045be6b476cb302d20a59270c014aca
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-11-19 14:16:34 +0800

    feat(install): add react-router-dom
    Used react-router-dom to create routing navigation and URL management for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./pages/home/LoginForm";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/home/LoginForm.jsx
+++ client/src/pages/home/LoginForm.jsx
@@ -1,23 +1,45 @@
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
+import { CiLock } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
 
 function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  }
+
   return (
-    <div className="flex flex-col items-center">
-      <span>Log In</span>
-      <div className="grid grid-rows-3 w-2xs">
-        <div className="">
-          <span>Email</span>
-          <Input />
+    <div className="flex flex-col items-center gap-5 justify-center h-screen">
+      <span className="font-semibold text-2xl text-neutral-600">Welcome</span>
+      <form className="grid grid-rows-3 w-2xs gap-6">
+        <div>
+          <span className="flex items-center gap-1 text-neutral-500">
+            <CiUser />
+            Email
+          </span>
+          <Input value={email} onChange={handleChangeEmail} className="text-neutral-700" />
         </div>
         <div>
-          <span>Password</span>
-          <Input />
+          <span className="flex items-center gap-1 text-neutral-500">
+            <CiLock />
+            Password
+          </span>
+          <Input value={password} onChange={handleChangePassword} className="text-neutral-700" />
         </div>
-        <div>
+        <div
+          className="text-center border border-blue-200 h-max rounded-md p-1 bg-blue-200
+        text-blue-400 font-bold text-lg"
+        >
           <button>Log In</button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
