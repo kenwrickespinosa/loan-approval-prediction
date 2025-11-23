4562f41df64186cb28779c24378c5dd70f7c79f2
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-11-23 17:52:07 +0800

    Added a route for index and store from ClientController

--- server-api/routes/api.php
+++ server-api/routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::post('/logout', [AuthController::class, 'logout']);
-//     Route::get('/user', function (Request $request) {
-//         return $request->user();
-//     });
-// });
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/clients', [ClientController::class, 'index']);
+    Route::post('/clients', [ClientController::class, 'store']);
+});
