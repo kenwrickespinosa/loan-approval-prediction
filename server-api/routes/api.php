9fc5e6ae821fbed3520dc7f5ad8f8d3bc6efff14
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-12-23 17:05:25 +0800

    feat(routes): add routes both for Client and Loan
    - Added endpoint route that returns the number of clients and clients with loans
    
    - Added endpoint route for predicting loan, total amount requested, and total loan status

--- server-api/routes/api.php
+++ server-api/routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\PredictController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -9,4 +11,13 @@ Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logo
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('/clients', [ClientController::class, 'index']);
     Route::post('/clients', [ClientController::class, 'store']);
+    Route::get('/clients/count', [ClientController::class, 'count']);
+    Route::get('/clients/clients-with-and-without-loans', [ClientController::class, 'clientsWithAndWithoutLoans']);
 });
+
+// Route::middleware('auth:sanctum')->post('/predict-loan', [PredictController::class, 'predict']);
+Route::post('/predict-loan', [PredictController::class, 'predict']);
+
+Route::post('/loan', [LoanController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/loan/total-amount-requested', [LoanController::class, 'totalAmountReq']);
+Route::middleware('auth:sanctum')->get('/loan/total-loan-status', [LoanController::class, 'totalLoanStatus']);
